52a60d0d06a74e7eaf7ae03370feed18e46c0266
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-07 16:36:06 +0100

    changed hello

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("i = " + i);
         }
 
-        System.out.println("hello from basel");
+        System.out.println("hello from basel, Hello world");
 
         System.out.println("hello from chrissy");
 
